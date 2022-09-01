5fa1de6832364ddbf3fea73fcced6c3d682c1080
JacobRigney <JRigney6993@gmail.com>
2022-08-31 18:02:47 -0700

    fix stuff

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+// converter function
 function converter(value, method) {
     if (method === 1) {
+        // number to binary string
         return value.toString(2);
     }else if (method === 2) {
+        // number to hex string
         return value.toString(16);
     }
 }
-let type = Number(prompt("Do you want to do 1 (binary) or 2 (hexadecimal) conversion?"));
-let number = Number(prompt("Enter the number to be converted."))
-console.log(converter(number,type))
+
+// normal
+while (true) {
+    let type = Number(prompt("Do you want to do 1. binary or 2. hexadecimal conversion?"));
+    let number = Number(prompt("Enter the number to be converted."))
+    let returned = converter(number,type)
+    // check if returned value is undefined or print the result
+    if (returned == undefined) {
+        console.log("Please read the documentation next time!");
+    } else {
+        console.log(returned);
+
+    }
+}
