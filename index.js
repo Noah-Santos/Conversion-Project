7bc034c41f8675ee847d8c5911253bb8dc2a900b
NoahSantos <nsanto591@west-mec.org>
2022-09-01 16:59:43 -0700

    Binary Converter
    Converts the user's entered number into a binary form with 8 digits.

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ function converter(value, method) {
 let type = Number(prompt("Do you want to do 1 (binary) or 2 (hexadecimal) conversion?"));
 let number = Number(prompt("Enter the number to be converted."))
 console.log(converter(number,type))
+
+//converts the user's entered number into binary with 8 digits
+let numbers = Number(prompt("Enter the number"));
+let result = numbers.toString(2);
+result = String(result);
+function convert(numb){
+    let num = numb;
+    while(num.length <9){
+        num = "0" + num;
+        if(num.length == 8){
+        return num;
+        }
+    }
+}
+alert(convert(result));
